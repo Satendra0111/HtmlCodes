db4a6950085b297d8d4646fff504059ed023c046
Sam12993a <gragerssam@gmail.com>
2023-11-04 22:57:33 +0530

    Sample Codes

---
+++ backup.js
@@ -0,0 +1,30 @@
+if (feedback1 > 4) {
+    console.log(name1 + " Given Good feedback");
+
+   } else {
+      console.log(name1 +" The feedback is bad");
+   }
+   if (feedback2 > 4) {
+       console.log(name2 + " Given Good feedback");
+
+   } else {
+         console.log(name2 +" The feedback is bad");
+    }
+   if (feedback3 > 4) {
+       console.log(name3 + " Given Good feedback");
+
+   } else {
+         console.log(name3 +" The feedback is bad");
+   }
+   if (feedback4 > 4) {
+       console.log(name4 + "Given Good feedback");
+
+   } else {
+         console.log(name4 +" The feedback is bad");
+   }
+   if (feedback5 > 4) {
+       console.log(name5 + " Given Good feedback");
+
+   } else {
+         console.log(name5 +" The feedback is bad");
+   }
